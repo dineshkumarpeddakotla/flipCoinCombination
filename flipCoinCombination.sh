3917b377e9815d1fa15776c4955597fdcb68cc9a
“dinnu247” <dineshkumar.icon@gmail.com>
2021-03-11 07:54:11 +0530

    [dinesh] added singlet flipCoinCombination.sh

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+declare -A flip
 random(){
 if [ $(( RANDOM%2 )) -eq 0 ]
 then
@@ -7,6 +8,12 @@ else
 echo T
 fi
 }
-
+for(( i=0;i<5;i++ ))
+do
+f=$( random )
+flip[$i]=$f
+winper=$(( 100/2 ))
+done
+echo ${flip[@]}
 
 
